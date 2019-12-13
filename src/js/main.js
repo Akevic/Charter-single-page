f4e48bb9f627eb43158ece75a496dda58f98ae66
Andrija <andrija@pointjupiter.com>
2019-12-13 11:28:23 +0100

    add tiny slider

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,10 @@
 document.querySelector('.nav-hamburger-menu').addEventListener('click', () => {
   document.querySelector('.nav-mobile-links').classList.toggle('show-hide')
 })
+
+const slider = tns({
+  container: '.my-slider',
+  items: 3,
+  slideBy: 'page',
+  autoplay: true
+});
